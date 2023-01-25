25583b9602dbc61d639c7d135b4377bdf2082809
ayushsachan123 <ayushsachan.in@gmail.com>
2023-01-25 19:28:22 +0530

    add bcrypt level 4 Security

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -20,9 +20,6 @@ const userSchema = new mongoose.Schema({
     email:String,
     password : String
 });
-
-//Use before mongoose model
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"] });
 
 const User = new mongoose.model("User",userSchema);
 
@@ -40,10 +37,11 @@ app.get("/register",function(req,res){
 
 app.post("/register",function(req,res){
 
-    const newUser = new User({
-       email: req.body.username,
-       password:req.body.password 
-    });
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password:hash
+         });
 
     newUser.save(function(err){
         if(err){
@@ -53,6 +51,8 @@ app.post("/register",function(req,res){
             res.render("secrets");
         }
     })
+    });
+
 });
 
 app.post("/login",function(req,res){
@@ -65,9 +65,11 @@ app.post("/login",function(req,res){
         }
         else{
             if(foundUser){
-                if(foundUser.password == password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password,function(err,result){
+                    if(result == true){
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
