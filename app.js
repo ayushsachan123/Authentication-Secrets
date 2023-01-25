7d95d95aceb2a80164417abfced1673c7782ae49
ayushsachan123 <ayushsachan.in@gmail.com>
2023-01-25 19:19:49 +0530

    add md5 level 3 Security

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,9 +19,6 @@ const userSchema = new mongoose.Schema({
     email:String,
     password : String
 });
-
-//Use before mongoose model
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"] });
 
 const User = new mongoose.model("User",userSchema);
 
@@ -42,7 +38,7 @@ app.post("/register",function(req,res){
 
     const newUser = new User({
        email: req.body.username,
-       password:req.body.password 
+       password:md5(req.body.password) 
     });
 
     newUser.save(function(err){
@@ -57,7 +53,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email:username},function(err,foundUser){
         if(err){
